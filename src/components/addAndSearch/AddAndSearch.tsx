85876a3292d7b6c28023f5e552a7cd763ca89190
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2025-02-06 15:38:56 +0100

    Lock focus on form

--- src/components/addAndSearch/AddAndSearch.tsx
+++ src/components/addAndSearch/AddAndSearch.tsx
@@ -1,35 +1,45 @@
-import { ChangeEvent, useContext } from 'react';
-import { CurrentStockType } from '../../hooks/useSeed';
-import styles from './AddAndSearch.module.css'
-import { SeedsContext } from '../../context/SeedsContext';
+import { ChangeEvent, useContext } from "react";
+import { CurrentStockType } from "../../hooks/useSeed";
+import styles from "./AddAndSearch.module.css";
+import { SeedsContext } from "../../context/SeedsContext";
 
 type addAndSearchProps = {
 	setFormOpen: React.Dispatch<React.SetStateAction<boolean>>;
 	setListToDisplay: React.Dispatch<React.SetStateAction<CurrentStockType>>;
-}
+};
 
+const AddAndSearch = ({ setFormOpen, setListToDisplay }: addAndSearchProps) => {
+	const { currentStock } = useContext(SeedsContext);
 
-const AddAndSearch = ({setFormOpen, setListToDisplay}: addAndSearchProps) => {
-	const { currentStock } = useContext(SeedsContext)
-
-	const handleSearchChange = (event: ChangeEvent<HTMLInputElement>)=> {	
+	const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
 		const searchValue = event.target.value;
-		if(searchValue === "") {
+		if (searchValue === "") {
 			setListToDisplay(currentStock);
 		} else {
-			setListToDisplay(currentStock.filter((
-				seed)=> seed.name.toLowerCase().includes(searchValue.toLowerCase())
-					|| 
-					seed.manufacturer.toLowerCase().includes(searchValue.toLowerCase())))
+			setListToDisplay(
+				currentStock.filter(
+					(seed) =>
+						seed.name
+							.toLowerCase()
+							.includes(searchValue.toLowerCase()) ||
+						seed.manufacturer
+							.toLowerCase()
+							.includes(searchValue.toLowerCase())
+				)
+			);
 		}
-	}
+	};
 
-  return (
-	<div className={styles.addAndSearchContainer}>
-		<button onClick={()=> setFormOpen(true)}>Nytt frø</button>
-		<input onChange={handleSearchChange} type="search" placeholder="Søk etter frø"/>
-	</div>
-  )
-}
+	return (
+		<div className={styles.addAndSearchContainer}>
+			<button onClick={() => setFormOpen(true)}>Nytt frø</button>
+			<input
+				onChange={handleSearchChange}
+				type="search"
+				placeholder="Søk etter frø"
+			/>
+		</div>
+	);
+};
 
-export default AddAndSearch
+export default AddAndSearch;
--- src/components/registerSeed/RegisterSeed.tsx
+++ src/components/registerSeed/RegisterSeed.tsx
@@ -4,12 +4,13 @@ import { useForm } from "react-hook-form";
 import { useContext } from "react";
 import { SeedsContext } from "../../context/SeedsContext";
 import { itemType } from "../../hooks/useSeed";
+import FocusLock from "react-focus-lock";
 
 type RegisterSeedProps = {
-	setFormOpen: React.Dispatch<React.SetStateAction<boolean>>,
+	setFormOpen: React.Dispatch<React.SetStateAction<boolean>>;
 	seedToEdit: itemType | null;
 	setSeedToEdit: React.Dispatch<React.SetStateAction<itemType | null>>;
-}
+};
 
 type Inputs = {
 	firestoreID: string;
@@ -19,74 +20,116 @@ type Inputs = {
 	comment: string;
 };
 
-const RegisterSeed = ({ setFormOpen, seedToEdit, setSeedToEdit }: RegisterSeedProps) => {
-	const { addSeed, editSeed } = useContext(SeedsContext)
-	const {register, handleSubmit, formState: { errors }} = useForm<Inputs>({		
+const RegisterSeed = ({
+	setFormOpen,
+	seedToEdit,
+	setSeedToEdit,
+}: RegisterSeedProps) => {
+	const { addSeed, editSeed } = useContext(SeedsContext);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<Inputs>({
 		defaultValues: {
-			name: seedToEdit ? seedToEdit.name : '',
-			manufacturer: seedToEdit ? seedToEdit.manufacturer : '',
-			stock: seedToEdit ? seedToEdit.stock : 'Hel',
-			comment: seedToEdit ? seedToEdit.comment : ''
+			name: seedToEdit ? seedToEdit.name : "",
+			manufacturer: seedToEdit ? seedToEdit.manufacturer : "",
+			stock: seedToEdit ? seedToEdit.stock : "Hel",
+			comment: seedToEdit ? seedToEdit.comment : "",
 		},
-		
 	});
 
 	const handleClose = () => {
-		setFormOpen(false)
-		setSeedToEdit(null)
-	}
+		setFormOpen(false);
+		setSeedToEdit(null);
+	};
 
 	const onSubmit = async (data: Inputs) => {
 		if (seedToEdit) {
-		  // If editing an existing seed
-		  editSeed({
-			...data,
-			firestoreID: seedToEdit.firestoreID // Use existing Firestore document ID
-		  });
+			// If editing an existing seed
+			editSeed({
+				...data,
+				firestoreID: seedToEdit.firestoreID, // Use existing Firestore document ID
+			});
 		} else {
-		  // If adding a new seed
-		  addSeed(data); // Firestore will generate the ID here
+			// If adding a new seed
+			addSeed(data); // Firestore will generate the ID here
 		}
 		setFormOpen(false);
 		setSeedToEdit(null); // Reset after adding or editing
-	  };
-
+	};
 
 	return (
-		<section className={style.registerSeed__wrapper}>
+		<FocusLock>
+			<section className={style.registerSeed__wrapper}>
 				<button
 					onClick={handleClose}
 					className={style.registerSeed__closeButton}
 				>
-					<img src={closeIcon} alt="close icon"/>
+					<img src={closeIcon} alt="close icon" />
 				</button>
-			<form onSubmit={handleSubmit(onSubmit)} className={style.registerSeed__form} action="">
-				<div>
-					<label htmlFor="name">Navn:</label>
-					<input type="text" id="name" autoFocus {...register("name", {required: "Navn er påkrevd", maxLength: {value: 20, message: "Navn kan være maks 20 tegn"}})} />
-					<div className={style.formError}>{errors.name?.message}</div>
-				</div>
-				<div>
-					<label htmlFor="manufacturer">Produsent:</label>
-					<input type="text" id="manufacturer" {...register("manufacturer", {required: "Produsent er påkrevd", maxLength: {value: 20, message: "Produsent kan være maks 20 tegn"}})} />
-					<div className={style.formError}>{errors.manufacturer?.message}</div>
-				</div>
-				<div>
-					<label htmlFor="stock">Beholdning:</label>
-					<select defaultValue="Hel" {...register("stock")}>
-						<option value="Rest">Rest</option>
-						<option value="Halv">Halv pakke</option>
-						<option value="Hel">Hel pakke</option>
-						<option value="Flere">Flere pakker</option>
-					</select>
-				</div>
-				<div>
-					<label htmlFor="comment">Kommentar:</label>
-					<input type="text" id="comment" {...register("comment")} />
-				</div>
-				<button type="submit">{seedToEdit ? 'Oppdater' : 'Legg til'}</button>
-			</form>
-		</section>
+				<form
+					onSubmit={handleSubmit(onSubmit)}
+					className={style.registerSeed__form}
+					action=""
+				>
+					<div>
+						<label htmlFor="name">Navn:</label>
+						<input
+							type="text"
+							id="name"
+							{...register("name", {
+								required: "Navn er påkrevd",
+								maxLength: {
+									value: 20,
+									message: "Navn kan være maks 20 tegn",
+								},
+							})}
+						/>
+						<div className={style.formError}>
+							{errors.name?.message}
+						</div>
+					</div>
+					<div>
+						<label htmlFor="manufacturer">Produsent:</label>
+						<input
+							type="text"
+							id="manufacturer"
+							{...register("manufacturer", {
+								required: "Produsent er påkrevd",
+								maxLength: {
+									value: 20,
+									message: "Produsent kan være maks 20 tegn",
+								},
+							})}
+						/>
+						<div className={style.formError}>
+							{errors.manufacturer?.message}
+						</div>
+					</div>
+					<div>
+						<label htmlFor="stock">Beholdning:</label>
+						<select defaultValue="Hel" {...register("stock")}>
+							<option value="Rest">Rest</option>
+							<option value="Halv">Halv pakke</option>
+							<option value="Hel">Hel pakke</option>
+							<option value="Flere">Flere pakker</option>
+						</select>
+					</div>
+					<div>
+						<label htmlFor="comment">Kommentar:</label>
+						<input
+							type="text"
+							id="comment"
+							{...register("comment")}
+						/>
+					</div>
+					<button type="submit">
+						{seedToEdit ? "Oppdater" : "Legg til"}
+					</button>
+				</form>
+			</section>
+		</FocusLock>
 	);
 };
 
